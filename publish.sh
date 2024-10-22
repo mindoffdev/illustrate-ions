9a2ed3e8118eb990c0a9955b6c8714c09143fbe6
Joel Samraj <joelsamrajs@gmail.com>
2024-10-23 01:02:23 +0530

    Auto commit: 2024-10-23 01:02:22

--- publish.sh
+++ publish.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+# Run the Python file
+python imagecompress-inator.py  # Assuming script.py is in the same folder as the shell script
 
+# Git commands
 git add .
 git commit -m "Auto commit: $(date +"%Y-%m-%d %H:%M:%S")"
 git push
